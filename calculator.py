6b1f763a84715537187c0adb6244c52fae8c24cd
lavanya770 <147373921+lavanya770@users.noreply.github.com>
2025-11-14 16:08:56 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+print("\n===== SIMPLE CALCULATOR =====")
+num1=int(input("Enter first number:"))
+operator=(input("Enter operation (+,-,*,/,%):"))
+num2=int(input("Enter second number:"))
+if operator=="+":
+  result=num1+num2
+elif operator=="-":
+    result=num1-num2
+elif operator=="*":
+      result=num1*num2
+elif operator=="/":
+        result=num1/num2
+elif operator=="%":
+    result=num1%num2
+else:
+  result="Invalid Operator"
+print("Result:", result)
+  
